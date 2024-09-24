5395fd16ca39b7c6e5778b95ef57e9268964b7bb
slord30 <sarahlord79@gmail.com>
2024-09-24 12:48:16 -0500

    added event handler to code

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,21 +3,30 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-//create a li element that will hold each entries chapter title and an associated delete button
-const li = document.createElement('li');
+//create a function that will run in response to the button being clicked
+button.addEventListener('click', () => {
+    //store what the user inputs in a variable
+    const myChapter = input.value;
+    //empty the input field by setting its value to an empty string ""
+    input.value = "";
 
-//create a delet button
-const deleteBtn = document.createElement('button');
+    //create 3 elements - list, span (text), and button (delete button) and store them in variables
+    const listItem = document.createElement('li'); //list created once user starts inputing chapters
+    const listText = document.createElement('span'); //bom chapters the user inputs
+    const deleteBtn = document.createElement('button'); //button appearing next to chapters to be able to delete them
 
-//populate the li element variable's textContent or innerHTML with the input value
-li.textContent = input.value;
+    //append the span(the input from the user) and button as children of the list item (list being created from user's input)
+    listItem.appendChild(listText); //append span to list item
+    listText.textContent = myChapter; //set the text content of th span to the input element value saved earlier (myChapter)
+    listItem.appendChild(deleteBtn); //append button to the list item
+    deleteBtn.textContent = '❌'; //set the text content of the button to a red x
+    list.appendChild(listItem); //append the list item as chilf of the list
 
-//populate the button textContent with a ❌
-deleteBtn.textContent = '❌';
+    //attach an event handler to the delete button so when clicked it will delete the bom chapter
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
 
-//append the li element variable with the delete button
-li.append(deleteBtn);
-
-//append the li element variable to the unordered list in your HTML
-li.appendChild(li);
-
+    //use focus method to focus the input element ready for enter the next favorite chapter 
+    input.focus();
+});
