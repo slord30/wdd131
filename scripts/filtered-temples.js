8efa0e21e2fc9b55b13cc6e37b71b153f7d25603
slord30 <sarahlord79@gmail.com>
2024-10-11 10:23:50 -0500

    fixed hamburger menu unresponsiveness

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -2,8 +2,10 @@ const d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
+
 const h2 = document.querySelector('h2')
 
+const navigation = document.querySelector('.navigation')
 const hamButton = document.querySelector('#menu');
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
